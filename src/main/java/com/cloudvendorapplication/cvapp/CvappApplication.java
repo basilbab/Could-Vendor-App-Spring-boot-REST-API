d0e68ba4f9b98bf264656eb3efe2141015b8f9f0
basilbab <basilbab@gmail.com>
2024-06-10 10:45:52 +0530

    first commit

---
+++ src/main/java/com/cloudvendorapplication/cvapp/CvappApplication.java
@@ -0,0 +1,13 @@
+package com.cloudvendorapplication.cvapp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CvappApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CvappApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/cloudvendorapplication/cvapp/CvappApplicationTests.java
@@ -0,0 +1,13 @@
+package com.cloudvendorapplication.cvapp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CvappApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
